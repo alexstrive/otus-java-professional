c605ed14722e6547b3625616538d9b23c2dfc987
Alexey Novopashin <alexei.novopashin@gmail.com>
2024-05-04 17:51:21 +0300

    complete hw01-gradle

---
+++ hw01-gradle/src/main/java/com/alexstrive/HelloOtus.java
@@ -0,0 +1,9 @@
+package com.alexstrive;
+
+import com.google.common.math.IntMath;
+
+public class HelloOtus {
+    public static void main(String... args) {
+        System.out.println(IntMath.binomial(20, 10));
+    }
+}
